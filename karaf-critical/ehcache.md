2f55dfd6253fb4c7ddf86815839eb18af5352563
Mike Hummel <mike.hummel@billingsolutions.de>
2020-03-26 21:09:26 +0100

    ehcache tests

---
+++ karaf-critical/ehcache.md
@@ -0,0 +1,52 @@
+
+
+### Start terracotta
+
+https://hub.docker.com/r/terracotta/terracotta-server-oss
+
+```
+docker run --name karaf-tc -p 9410:9410 -d terracotta/terracotta-server-oss:5.6.4
+```
+
+### Start karaf 1:
+
+```
+docker run -it \
+ -v ~/.m2:/home/user/.m2\
+ --link karaf-tc:tcserver\
+ -p 15006:5005\
+ -p 18182:8181\
+ --name karaf1\
+ mhus/apache-karaf:4.2.6_04 debug
+```
+
+### Start karaf 2:
+
+```
+docker run -it \
+ -v ~/.m2:/home/user/.m2\
+ --link karaf-tc:tcserver\
+ -p 15007:5005\
+ -p 18183:8181\
+ --name karaf2\
+ mhus/apache-karaf:4.2.6_04 debug
+```
+
+### Install
+
+And install software in both environments
+
+```
+install -s mvn:javax.cache/cache-api/1.1.1
+install -s mvn:org.ehcache/ehcache/3.8.1
+install -s mvn:org.ehcache/ehcache-clustered/3.8.1
+
+feature:repo-add mvn:org.apache.shiro/shiro-features/1.5.1/xml/features
+feature:repo-add mvn:de.mhus.osgi/karaf-features/7.0.0-SNAPSHOT/xml/features
+feature:install mhu-dev-critical
+
+# mhus:dev -y cp jms ENV_JMS_SOP_USER=admin ENV_JMS_SOP_PASS=nein ENV_JMS_SERVER=jmsserver:61616
+
+bundle:persistentwatch add .*
+```
+
--- karaf-critical/src/main/java/de/mhus/osgi/dev/critical/CmdDevEhcache.java
+++ karaf-critical/src/main/java/de/mhus/osgi/dev/critical/CmdDevEhcache.java
@@ -1,18 +1,28 @@
 package de.mhus.osgi.dev.critical;
 
-import javax.cache.Cache;
-import javax.cache.CacheManager;
-import javax.cache.configuration.MutableConfiguration;
+import static java.net.URI.create;
+import static org.ehcache.clustered.client.config.builders.ClusteredResourcePoolBuilder.clusteredDedicated;
+import static org.ehcache.clustered.client.config.builders.ClusteringServiceConfigurationBuilder.cluster;
+import static org.ehcache.config.builders.CacheConfigurationBuilder.newCacheConfigurationBuilder;
+import static org.ehcache.config.builders.CacheManagerBuilder.newCacheManagerBuilder;
+import static org.ehcache.config.builders.ResourcePoolsBuilder.heap;
+import static org.ehcache.config.units.MemoryUnit.MB;
+
+import java.net.URI;
+import java.util.ServiceLoader;
+
+import javax.cache.Caching;
+import javax.cache.spi.CachingProvider;
 
 import org.apache.karaf.shell.api.action.Argument;
 import org.apache.karaf.shell.api.action.Command;
 import org.apache.karaf.shell.api.action.lifecycle.Service;
+import org.ehcache.Cache;
+import org.ehcache.CacheManager;
+import org.ehcache.core.Ehcache;
+import org.ehcache.sample.CreateBasicJCacheProgrammatic;
 
-import com.hazelcast.cache.HazelcastCachingProvider;
-import com.hazelcast.cache.impl.HazelcastServerCachingProvider;
-import com.hazelcast.core.Hazelcast;
-import com.hazelcast.core.HazelcastInstance;
-
+import de.mhus.lib.core.MCast;
 import de.mhus.osgi.api.karaf.AbstractCmd;
 
 @Command(scope = "mhus", name = "dev-ehcache", description = "Ehcache tests")
@@ -24,7 +34,11 @@ public class CmdDevEhcache extends AbstractCmd {
             name = "cmd",
             required = true,
             description =
-                    "Command to execute"
+                    "Command to execute\n"
+                    + "  basic\n"
+                    + "  clustered terracotta://tcserver:9410/clustered offheap-1 [key long] [value string] - set\n"
+                    + "  clustered terracotta://tcserver:9410/clustered offheap-1 [key long] - get\n"
+                    + ""
             ,
             multiValued = false)
     String cmd;
@@ -37,33 +51,76 @@ public class CmdDevEhcache extends AbstractCmd {
             multiValued = true)
     String[] parameters;
     
-    static HazelcastServerCachingProvider provider = null;
-    static CacheManager cacheManager = null;
-    
     @Override
     public Object execute2() throws Exception {
 
 
+        if (cmd.equals("jsr107.base")) {
+            if (parameters == null) parameters = new String[0];
+            
+            CreateBasicJCacheProgrammatic.main(parameters);
+
+            return null;
+        }
+
+        if (cmd.equals("clustered")) {
+            log().i("Creating clustered cache manager");
+            // terracotta://tcserver:9410/clustered
+            // default-resource
+            URI uri = create(parameters[0]);
+            try (CacheManager cacheManager = newCacheManagerBuilder()
+                    .with(cluster(uri).autoCreate().defaultServerResource(parameters[1]))
+                    .withCache("basicCache",
+                            newCacheConfigurationBuilder(Long.class, String.class,
+                                    heap(100).offheap(1, MB).with(clusteredDedicated(5, MB))))
+                    .build(true)) {
+              Cache<Long, String> basicCache = cacheManager.getCache("basicCache", Long.class, String.class);
+
+              log().i("Putting to cache");
+              basicCache.put(1L, "da one!");
+
+              System.out.println("1: " + basicCache.get(1L) );
+              if (parameters.length == 3) {
+                  long k = MCast.tolong(parameters[2], 0);
+                  System.out.println( k + ": " + basicCache.get(k) );
+              } else
+              if (parameters.length == 4) {
+                  long k = MCast.tolong(parameters[2], 0);
+                  String v = parameters[3];
+                  System.out.println("Set " + k + " to " + v);
+                  basicCache.put(k, v);
+              }
+              
+              log().i("Closing cache manager");
+            }
+
+            log().i("Exiting");        }
+        
+        
+        if (cmd.equals("basic")) {
+            
+            log().i("Creating cache manager programmatically");
+            try (CacheManager cacheManager = newCacheManagerBuilder()
+              .withCache("basicCache",
+                newCacheConfigurationBuilder(Long.class, String.class, heap(100).offheap(1, MB)))
+              .build(true)) {
+              Cache<Long, String> basicCache = cacheManager.getCache("basicCache", Long.class, String.class);
+
+              log().i("Putting to cache");
+              basicCache.put(1L, "da one!");
+              String value = basicCache.get(1L);
+              log().i("Retrieved '{}'", value);
+
+              log().i("Closing cache manager");
+            }
+
+            log().i("Exiting");
+            
+        }
+
         return null;
     }
-
-//    protected <K, V, C extends Configuration<K, V>> CacheConfig<K, V> createCacheConfig(String cacheName,
-//            C configuration) {
-//        CacheConfig<K, V> cacheConfig;
-//        if (configuration instanceof CompleteConfiguration) {
-//        cacheConfig = new CacheConfig<K, V>((CompleteConfiguration) configuration);
-//        } else {
-//        cacheConfig = new CacheConfig<K, V>();
-//        cacheConfig.setStoreByValue(configuration.isStoreByValue());
-//        final Class<K> keyType = configuration.getKeyType();
-//        final Class<V> valueType = configuration.getValueType();
-//        cacheConfig.setTypes(keyType, valueType);
-//        }
-//        cacheConfig.setName(cacheName);
-//        cacheConfig.setManagerPrefix(cacheNamePrefix);
-//        cacheConfig.setUriString(getURI().toString());
-//        return cacheConfig;
-//    }
     
+
     
 }
