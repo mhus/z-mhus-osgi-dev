4229ff4e1471be7d4aca752b783d5f2fe3ae2cb8
Mike Hummel <mike@mhus.de>
2020-05-11 22:21:55 +0200

    add k8s dev project

---
+++ dev-k8s/src/main/java/de/mhus/osgi/dev/k8s/impl/CmdDevK8s.java
@@ -0,0 +1,46 @@
+package de.mhus.osgi.dev.k8s.impl;
+
+import org.apache.karaf.shell.api.action.Argument;
+import org.apache.karaf.shell.api.action.Command;
+import org.apache.karaf.shell.api.action.lifecycle.Service;
+
+import de.mhus.osgi.api.karaf.AbstractCmd;
+
+// https://github.com/kubernetes-client/java/tree/master/examples/src/main/java/io/kubernetes/client/examples
+
+
+@Command(scope = "mhus", name = "dev-k8s", description = "Kubernetes tests")
+@Service
+public class CmdDevK8s extends AbstractCmd {
+
+    @Argument(
+            index = 0,
+            name = "cmd",
+            required = true,
+            description =
+                    "Command to execute\n"
+                    + "  list"
+            ,
+            multiValued = false)
+    String cmd;
+
+    @Argument(
+            index = 1,
+            name = "paramteters",
+            required = false,
+            description = "Parameters",
+            multiValued = true)
+    String[] parameters;
+        
+    @Override
+    public Object execute2() throws Exception {
+        
+    	if (cmd.equals("controller")) {
+    		ControllerExample.main(parameters);
+    		System.out.println("Done");
+    	}
+
+        return null;
+    }
+
+}
