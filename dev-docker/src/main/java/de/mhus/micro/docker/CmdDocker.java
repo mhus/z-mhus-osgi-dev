694f557877abba05dfff988ffdf58857271a3b63
Mike Hummel <mike.hummel@billingsolutions.de>
2020-07-03 08:27:52 +0200

    add docker api dev test

---
+++ dev-docker/src/main/java/de/mhus/micro/docker/CmdDocker.java
@@ -0,0 +1,51 @@
+package de.mhus.micro.docker;
+
+import java.io.File;
+
+import org.apache.karaf.shell.api.action.Argument;
+import org.apache.karaf.shell.api.action.Command;
+import org.apache.karaf.shell.api.action.lifecycle.Service;
+
+import com.amihaiemil.docker.Container;
+import com.amihaiemil.docker.Containers;
+import com.amihaiemil.docker.UnixDocker;
+
+import de.mhus.osgi.api.karaf.AbstractCmd;
+
+@Command(scope = "mhus", name = "dev-docker", description = "Docker")
+@Service
+public class CmdDocker extends AbstractCmd {
+
+    @Argument(
+            index = 0,
+            name = "cmd",
+            required = true,
+            description = "ps",
+            multiValued = false)
+    String cmd;
+    
+    @Argument(
+            index = 1,
+            name = "paramteters",
+            required = false,
+            description = "Parameters",
+            multiValued = true)
+    String[] parameters;
+
+    @Override
+    public Object execute2() throws Exception {
+        
+        if (cmd.equals("ps")) {
+            Containers list = new UnixDocker(new File("/var/run/docker.sock"))
+                    .containers();
+            for (Container c : list) {
+                System.out.println(c.containerId());
+                System.out.println(c);
+                System.out.println();
+            }
+         }
+
+        return null;
+    }
+
+}
