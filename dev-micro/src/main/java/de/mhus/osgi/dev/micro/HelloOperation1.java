ad6264de3e92e03b50124d1598bd5a609db75fcf
Mike Hummel <mike.hummel@billingsolutions.de>
2020-09-24 23:04:48 +0200

    Update HelloOperation1.java

--- dev-micro/src/main/java/de/mhus/osgi/dev/micro/HelloOperation1.java
+++ dev-micro/src/main/java/de/mhus/osgi/dev/micro/HelloOperation1.java
@@ -27,7 +27,7 @@ import de.mhus.lib.core.operation.TaskContext;
 import de.mhus.lib.core.service.IdentUtil;
 
 @Component(service = Operation.class)
-@OperationService(title = "Hello", path="de.mhus.osgi.dev.critical.micro.Hello",version="1.0.0")
+@OperationService(title = "Hello", path="de.mhus.osgi.dev.critical.micro.Hello",version="1.1.0")
 public class HelloOperation1 extends AbstractOperation {
 
     private String nextMsg = "Moin";
